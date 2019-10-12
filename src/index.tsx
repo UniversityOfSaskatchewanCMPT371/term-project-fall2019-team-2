f1f3d17e418a4aaa93f7478de0cd87826f088855
Braunson Mazoka <5280764+braunsonm@users.noreply.github.com>
2019-10-12 00:19:24 -0600

    Sonar not detecting obvious rules

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
+const password = "hey";
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

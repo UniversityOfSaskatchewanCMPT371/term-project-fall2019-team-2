78de7c4904625ff1ce87b93c4ce1bf72875924df
Braunson Mazoka <5280764+braunsonm@users.noreply.github.com>
2019-10-12 01:33:19 -0600

    Build Pipeline Improvements

--- src/index.tsx
+++ src/index.tsx
@@ -7,8 +7,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-const password = 'hey';
-
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

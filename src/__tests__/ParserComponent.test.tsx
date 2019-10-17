98f643d91208257f91a446b681fe2accdd7a2149
Braunson Mazoka <5280764+braunsonm@users.noreply.github.com>
2019-10-17 17:11:03 -0600

    Fixed some tests

--- src/__tests__/ParserComponent.test.tsx
+++ src/__tests__/ParserComponent.test.tsx
@@ -18,9 +18,10 @@ describe('<ParserComponent /> renders correctly', () => {
   it('renders a <ParseComponent /> to select a .tl', () => {
     const wrapper = shallow(<ParserComponent prompt={'Select a TL file: '} fileType={FileType.tl}/>);
     const prompt = <label>Select a TL file: </label>;
-    const button = <input type='file' accept='.tl' />;
+    const button = <input type='file' accept='.tl' ></input>;
 
     expect(wrapper.contains(prompt)).toEqual(true);
-    // expect(wrapper.contains(button)).toEqual(true);
+    expect(wrapper.exists('input')).toEqual(true);
+    //expect(wrapper.contains(button)).toEqual(true);
   });
 });

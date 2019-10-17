2b210c885bc8a247e0233cd4a0c52b2f5660a279
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-10-17 16:47:46 -0600

    added the rest of the testing files in __tests__

---
+++ src/__tests__/Filter.test.tsx
@@ -0,0 +1,7 @@
+import React, {ReactDOM} from 'react';
+import {shallow} from 'enzyme';
+import Filter from '../components/Filter';
+
+describe('<Filter /> renders successfully', () => {
+   // todo
+});
--- src/__tests__/ParserComponent.test.tsx
+++ src/__tests__/ParserComponent.test.tsx
@@ -3,17 +3,24 @@ import {shallow} from 'enzyme';
 import ParserComponent from '../components/ParserComponent';
 import {FileType} from '../components/ParserInterface';
 
-describe('<ParserComponent />', () => {
-  it('renders a <ParserComponent />', () => {
-    const wrapper = shallow(<ParserComponent prompt={'Select a CSV file: '}
-                                             fileType={FileType.csv}/>);
+describe('<ParserComponent /> renders correctly', () => {
+  it('renders a <ParserComponent /> to select a .csv', () => {
+    const wrapper = shallow(<ParserComponent prompt={'Select a CSV file: '} fileType={FileType.csv}/>);
     const prompt = <label>Select a CSV file: </label>;
-    // @ts-ignore
-    const button = <input type="file" accept=".csv,text/csv"/>;
+    // const button = <input type="file" accept=".csv,text/csv"/>;
 
 
     expect(wrapper.contains(prompt)).toEqual(true);
     // input tag not being rendered properly >:c
+    // expect(wrapper.contains(button)).toEqual(true);
+  });
+
+  it('renders a <ParseComponent /> to select a .tl', () => {
+    const wrapper = shallow(<ParserComponent prompt={'Select a TL file: '} fileType={FileType.tl}/>);
+    const prompt = <label>Select a TL file: </label>;
+    const button = <input type='file' accept='.tl' />;
+
+    expect(wrapper.contains(prompt)).toEqual(true);
     // expect(wrapper.contains(button)).toEqual(true);
   });
 });

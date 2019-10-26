2d84b2be376a6a069e2645eebf6535f851ab97f0
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-10-26 14:47:10 -0600

    changed double quotes to single

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import Mesa from './Mesa';
 import * as serviceWorker from './serviceWorker';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
-import App from "./App";
+import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 

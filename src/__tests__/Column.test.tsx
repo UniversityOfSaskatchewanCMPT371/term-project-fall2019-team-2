68280c9d6a7295a409beda6412bb82c179a6efcc
kevinwish <pez395@usask.ca>
2019-10-29 17:52:08 -0600

    deleted all the deprecated unit tests

--- src/__tests__/Column.test.tsx
+++
@@ -1,7 +0,0 @@
-import React, {ReactDOM} from 'react';
-import {shallow} from 'enzyme';
-import Column from '../components/Column';
-
-describe('<Column /> renders successfully', () => {
-  // todo
-});

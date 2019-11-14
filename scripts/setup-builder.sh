351290d061e7f95b862523816b65642e1a9e1bf2
Braunson Mazoka <5280764+braunsonm@users.noreply.github.com>
2019-11-14 14:48:27 -0600

    Removed TTY Interface

--- scripts/setup-builder.sh
+++ scripts/setup-builder.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y python3-pip && 
+# For use with the build.
+sudo apt update && 
+sudo apt install -y python3-pip && 
 cd /mnt && 
 pip3 install -r scripts/requirements.txt && 
 python3 scripts/smoke-test.py build/index.html

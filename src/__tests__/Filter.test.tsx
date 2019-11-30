2455ce83ad4e9e9967b643512a7ca2a6cec3fc7d
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-11-30 16:52:17 -0600

    wrote tests for constructors of Data & Filter (need at least 1 test in suite or it fails)

--- src/__tests__/Filter.test.tsx
+++ src/__tests__/Filter.test.tsx
@@ -3,4 +3,24 @@ import {shallow} from 'enzyme';
 import Filter from '../components/Filter';
 
 describe('<Filter /> Unit Tests', () => {
+  describe('constructor()', () => {
+    let filterObj: Filter;
+    const lowerRange: number = 1;
+    const upperRange: number = 5;
+    const isShown: boolean = true;
+    const listOfPreds: Array<(...args: any[]) => boolean> =
+        [() => true];
+
+
+    beforeEach(() => {
+      filterObj = new Filter(lowerRange, upperRange, isShown, listOfPreds);
+    });
+
+    it('constructor should assigns variables', () => {
+      expect(filterObj.lowerRange).toBe(lowerRange);
+      expect(filterObj.upperRange).toBe(upperRange);
+      expect(filterObj.isShown).toBeTruthy();
+      expect(filterObj.listOfPreds).toBe(listOfPreds);
+    });
+  });
 });

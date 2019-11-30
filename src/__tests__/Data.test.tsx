950f6e09dcf2ae8647d990ffaf4db03ff94ccb39
eileenvanheerde <eileen.vanheerde@usask.ca>
2019-11-30 16:21:34 -0600

    remove even more empty dummy tests

--- src/__tests__/Data.test.tsx
+++ src/__tests__/Data.test.tsx
@@ -3,9 +3,4 @@ import {shallow} from 'enzyme';
 import Data from '../components/Data';
 
 describe('<Data /> Unit Tests', () => {
-  describe('constructor()', () => {
-    it('dummy test', () => {
-      // todo: devs need to write unit tests
-    });
-  });
 });
--- src/__tests__/Filter.test.tsx
+++ src/__tests__/Filter.test.tsx
@@ -3,27 +3,4 @@ import {shallow} from 'enzyme';
 import Filter from '../components/Filter';
 
 describe('<Filter /> Unit Tests', () => {
-  describe('constructor()', () => {
-    it('dummy test', () => {
-      // todo: devs need to write unit tests
-    });
-  });
-
-  describe('redefineRange()', () => {
-    it('dummy test', () => {
-      // todo: devs need to write unit tests
-    });
-  });
-
-  describe('addPredicate()', () => {
-    it('dummy test', () => {
-      // todo: devs need to write unit tests
-    });
-  });
-
-  describe('removePredicate()', () => {
-    it('dummy test', () => {
-      // todo: devs need to write unit tests
-    });
-  });
 });
